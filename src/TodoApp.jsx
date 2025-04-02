820e09ac93644b438f37bfddc8ac4635ea0c0d42
caaeen <canadatofer@gmail.com>
2025-04-02 23:20:59 +0800

    second

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -57,7 +57,7 @@ const Todo = () => {
       
         <div className='fade-in  bg-[var(--primary-color)] todo-container relative p-5 min-h-svh h-full max-w-[800px] w-full'>
           <div className='flex justify-between'>
-            <h1 className='border-b-2 w-1/2'>To-Dos</h1>
+            <h1 className='border-b-2 w-1/2 sm:text-[2.3rem] text-[1.7rem]'>To-Dos</h1>
             <button
               onClick={() => openModal()} // open modal for adding tasks
               className='sm:top-5 sm:right-5 bottom-20 right-5 duration-300 ease-in bg-[var(--hc-color)] hover:bg-[var(--inc-color)] p-2 sm:w-[55px] sm:h-[55px] w-[50px] h-[50px] rounded-[50px]'
