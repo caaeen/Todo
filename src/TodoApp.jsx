e57048a6367a418a2a9db5ccf8bbb5b924e60e24
caaeen <canadatofer@gmail.com>
2025-04-03 11:32:06 +0800

    fifth

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -69,27 +69,27 @@ const Todo = () => {
           {/* Category */}
           <div className="w-full pt-5 flex items-center gap-4">
             <button
-              className={` max-w-[200px] px-3 py-1 rounded-[5px] sm:text-[1rem] text-[0.7rem] ${selectedCategory === "all" ? "bg-white text-black " : "hover:bg-white/40 ease duration-300"}`}
+              className={` max-w-[200px] px-3 py-1 rounded-[5px] ${selectedCategory === "all" ? "bg-white text-black " : "hover:bg-white/40 ease duration-300"}`}
               onClick={() => setSelectedCategory("all")}
             >
-              <p>All ({tasks.length})</p>
+              <p className='sm:text-[1rem] text-[0.8rem]'>All ({tasks.length})</p>
             </button>
             <button
-              className={`max-w-[200px] px-3 py-1 rounded-[5px] sm:text-[1rem] text-[0.7rem]${selectedCategory === "complete" ? "bg-white text-black " : "hover:bg-white/40 ease duration-300"}`}
+              className={`max-w-[200px] px-3 py-1 rounded-[5px]  ${selectedCategory === "complete" ? "bg-white text-black " : "hover:bg-white/40 ease duration-300"}`}
               onClick={() => setSelectedCategory("complete")}
             >
-              <p>Complete ({tasks.filter(task => task.isComplete).length})</p>
+              <p className='sm:text-[1rem] text-[0.8rem]'>Complete ({tasks.filter(task => task.isComplete).length})</p>
             </button>
             <button
-              className={`max-w-[200px] px-3 py-1 rounded-[5px] sm:text-[1rem] text-[0.7rem] ${selectedCategory === "incomplete" ? "bg-white text-black " : "hover:bg-white/40 ease duration-300"}`}
+              className={`max-w-[200px] px-3 py-1 rounded-[5px]  ${selectedCategory === "incomplete" ? "bg-white text-black " : "hover:bg-white/40 ease duration-300"}`}
               onClick={() => setSelectedCategory("incomplete")}
             >
-              <p>Incomplete ({tasks.filter(task => !task.isComplete).length})</p>
+              <p className='sm:text-[1rem] text-[0.8rem]'>Active ({tasks.filter(task => !task.isComplete).length})</p>
             </button>
           </div>
 
          
-          <div className=' overflow-x-hidden w-full h-full pt-10 pb-20 relative flex flex-col gap-5'>
+          <div className=' overflow-x-hidden w-full h-full sm:pt-10 pt-7 pb-20 relative flex flex-col gap-5'>
           {tasks.length === 0 ? (
             <NoToDo />
           ) : (
@@ -140,9 +140,9 @@ const Todo = () => {
                     </div>
                   </div>
                   <div className={`mt-3 ${task.isComplete ? "line-through " : "text-none"}`}>
-                    <p className='sm:text-[1rem] text-[0.8rem]'>{task.description}</p>
+                    <p className='sm:text-[1rem] text-[0.9rem]'>{task.description}</p>
                   </div>
-                  <div className="flex justify-between w-full mt-3">
+                  <div className="flex justify-between items-center w-full mt-3">
                     <div>
                     <p className='sm:text-[1rem] text-[0.7rem]'><b>Due: </b>{task.dueDate ? new Date(task.dueDate).toLocaleString("en-US", { 
                       year: 'numeric', 
